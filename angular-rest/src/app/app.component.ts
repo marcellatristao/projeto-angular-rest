12c178ff5619a0ce2dae693c96f5ebc80054e51b
MarcellaCanedo <marcellacanedo@hotmail.com>
2020-01-22 15:59:30 -0300

    Iniciando form login

--- angular-rest/src/app/app.component.ts
+++ angular-rest/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-rest';
+
+  usuario = {login: '', senha:''};
+
+  public login(){
+    console.log("Login: " + this.usuario.login  + "\nSenha: " + this.usuario.senha)
+  }
 }
