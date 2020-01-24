b75cb4fdb1c354527d4c71f8d77cc3dd9d092d9b
MarcellaCanedo <marcellacanedo@hotmail.com>
2020-01-23 21:55:36 -0300

    Autenticação JWT

---
+++ angular-rest/src/app/service/login-service.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AppConstants } from '../app-constants';
+import { error } from '@angular/compiler/src/util';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginServiceService {
+
+  constructor(private http: HttpClient) { }
+
+  login(usuario) {
+    return this.http.post(AppConstants.baseLogin, JSON.stringify(usuario)).subscribe(data => {
+
+      /*Retorno http*/
+
+      var token = JSON.parse(JSON.stringify(data)).Authorization.split(' ')[1];
+
+      localStorage.setItem("token", token);
+      //console.info("Token: " + localStorage.getItem("token")); /*Imprime o token no console*/
+
+    },
+      error => {
+        console.error("Erro ao fazer login");
+      }
+    );
+  }
+}
